7eaeb84b098b53f747489318e74d577c2e5a9479
Jacob Truelove <jet4853@mavs.uta.edu>
2025-05-27 11:25:20 -0500

    got home button working and project pages started

--- test.js
+++ test.js
@@ -1,8 +1,16 @@
 function switchIframe(){
-    if(document.getElementById("frame").src == "page2.html"){
-        document.getElementById("frame").src = "new.html";
-    }
-    else{
+    document.getElementById("test").innerHTML = "these words are too long";
+    if(document.getElementById("frame").src != "http://127.0.0.1:5500/page2.html"){
         document.getElementById("frame").src = "page2.html";
     }
+    else{
+        document.getElementById("frame").src = "page1.html";
+    }
+}
+
+function home(){
+    location.href= 'home.html';
+}
+function projects(){
+    location.href= 'projects.html';
 }
